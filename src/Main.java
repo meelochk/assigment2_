5c8c6f30c5a7b57c5b242db95ea9b48e28c1abc8
Ella <146346991+meelochk@users.noreply.github.com>
2024-01-26 21:24:57 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,26 @@
 import interfaces.Payable;
-
 import models.*;
 
 import java.util.ArrayList;
-
 import java.util.Collections;
 
-
 public class Main {
-    public static void printData(Iterable<Person> data){
-        for(Payable person : data){
-
+    public static void printData(Iterable<? extends Payable> data) {
+        for (Payable person : data) {
             System.out.println(person + " earns " + person.getPaymentAmount() + " tenge");
-
         }
     }
+
     public static void main(String[] args) {
-        ArrayList<Person> persons = new ArrayList<>();
+        ArrayList<Payable> payables = new ArrayList<>();
 
-        persons.add(new Employee("John", "Lennon", "Employee", 27045.78));
+        payables.add(new Employee("John", "Lennon", "Employee", 27045.78));
+        payables.add(new Employee("George", "Harrison", "Employee", 50000.0));
+        payables.add(new Student("Ringo", "Starr", 2.0));
+        payables.add(new Student("Paul", "McCartney", 3.0));
 
-        persons.add(new Employee("George", "Harrison", "Employee", 50000.0));
+        Collections.sort(payables, (p1, p2) -> Double.compare(p1.getPaymentAmount(), p2.getPaymentAmount()));
 
-        persons.add(new Student("Ringo", "Starr", 2.0));
-
-        persons.add(new Student("Paul", "McCartney", 3.0));
-
-        Collections.sort(persons);
-
-        printData(persons);
+        printData(payables);
     }
-
 }
